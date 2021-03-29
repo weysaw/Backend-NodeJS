f118380b8b6c8c14edf62033f381039d80e2ad58
Axel L. Ornelas M <axel.lom17@gmail.com>
2021-03-29 05:09:58 -0700

    Update habientebancaria.js

--- models/habientebancaria.js
+++ models/habientebancaria.js
@@ -3,13 +3,21 @@
 module.exports = (sequelize, DataTypes) => {
   const HabienteBancaria = sequelize.define('HabienteBancaria', {
     // Model attributes are defined here
-    habienteId: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
     bancariaId: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: false,
+      references: {
+        model: "CuentaBancaria",
+        key: 'id'
+      }
+    },
+    habienteId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "Habientes",
+        key: 'id'
+      }
     }
   }, {
     // Other model options go here
