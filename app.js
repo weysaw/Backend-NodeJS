571e72e179cde8a6301f89adf100b2cceaa98e15
Axel L. Ornelas M <axel.lom17@gmail.com>
2021-04-08 18:24:51 -0700

    Se cambio http por https
    Se agrego el código necesario para iniciar el servidor https

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
 //Declaración de variables 
 const express = require('express');
 const app = express();
+const fs = require("fs");
+const https = require("https");
 const router = require(`./routes/index`);
 const sequelize = require('./models').sequelize;
 
+const llavePrivada = fs.readFileSync("private.key");
+const certificado = fs.readFileSync("certificate.crt");
+const credenciales = {
+    key: llavePrivada,
+    cert: certificado,
+    passphrase: "1234" //passwd de la llave privada usado en la creación del certificado
+};
+
+
 //Configuración
-app.set('port', process.env.PORT || 3000);
+process.env.port = 4001;
 app.set('json spaces', 2);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 /**
  * RUTAS 
- */
- async function iniciar() {
+*/
+async function iniciar() {
     await sequelize.models.HabienteBancaria.drop({ force: true });
     await sequelize.models.CuentaBancaria.sync({ force: true });
-    await sequelize.models.Habiente.sync({ force: true });
+    await sequelize.models.CuentaHabiente.sync({ force: true });
     await sequelize.models.HabienteBancaria.sync({ force: true });
 }
 
@@ -27,15 +38,14 @@ iniciar();
  */
 app.use(router);
 
-
-
 /**
  * Inicia el servidor
  */
-const servidor = app.listen(app.get('port'), () => {
-    console.log(`Servidor en puerto ${app.get('port')}`);
+const httpsServer = https.createServer(credenciales, app);
+const servidor = httpsServer.listen(process.env.port, () => {
+    console.log('Servidor https escuchando por el puerto:', process.env.port);
 }).on('error', err => {
-    console.log("Error al iniciar el servidor", err);
+    console.log('Error al inciar el servidor:', err);
 });
 
 process.on('SIGINT', () => {
